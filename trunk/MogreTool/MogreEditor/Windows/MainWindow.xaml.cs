4ccebff2a26f5bb9d67999ac6ba75ec739d96f17
likeleon <likeleon@a2a70008-d901-af86-2ea6-1ad5e87d510c>
2010-04-02 13:09:57 +0000

    Ogre가 WPF 윈도우에 렌더링됨

--- trunk/MogreTool/MogreEditor/Windows/MainWindow.xaml.cs
+++ trunk/MogreTool/MogreEditor/Windows/MainWindow.xaml.cs
@@ -1,15 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+using Mogre;
 
 namespace MogreEditor.Windows
 {
@@ -20,7 +11,50 @@ namespace MogreEditor.Windows
     {
         public MainWindow()
         {
+            App.Current.Exit += Current_Exit;
+
             InitializeComponent();
         }
+
+        #region Private Methods
+
+        private void Current_Exit(object sender, ExitEventArgs e)
+        {
+            this.renderTargetControl.Source = null;
+
+            this.ogreImage.Dispose();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            this.ogreImage.InitOgreAsync();
+        }
+
+        private void renderTargetControl_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            if (this.ogreImage == null)
+                return;
+
+            this.ogreImage.ViewportSize = e.NewSize;
+        }
+
+        private void renderTargetControl_MouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            MessageBox.Show("Clicked");
+        }
+
+        private void ogreImage_Initialized(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void ogreImage_PreRender(object sender, EventArgs e)
+        {
+        }
+
+        private void ogreImage_ResourceLoadItemProgress(object sender, MogreEditor.Controls.ResourceLoadEventArgs e)
+        {
+
+        }
+        #endregion
     }
 }
