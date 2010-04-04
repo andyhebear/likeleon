1d704268c9efbd141634c466c5a58d8363a87337
likeleon <likeleon@a2a70008-d901-af86-2ea6-1ad5e87d510c>
2010-04-04 04:56:03 +0000

    Normal	completed	Mogre 메시지 추가시 자동 스크롤

--- trunk/MogreTool/MogreEditor/Controls/LogControl.cs
+++ trunk/MogreTool/MogreEditor/Controls/LogControl.cs
@@ -5,14 +5,19 @@ using System.ComponentModel;
 
 namespace Mogitor.Controls
 {
+    [TemplatePart(Name = "PART_LogStringsDisplay", Type = typeof(ListBox))]
     class LogControl : Control
     {
-        #region Fields
+        #region Dependency Properties
         /// <summary>
         /// Dependency Property backing store for LogBuffer
         /// </summary>
         public static readonly DependencyProperty LogBufferProperty =
             DependencyProperty.Register("LogBuffer", typeof(LogBuffer), typeof(LogControl), new UIPropertyMetadata(null));
+        #endregion
+
+        #region Private Fields
+        private ListBox logStringsDisplay;
         #endregion
 
         #region Public Properties
@@ -25,7 +30,19 @@ namespace Mogitor.Controls
             set 
             {
                 SetValue(LogBufferProperty, value);
+                LogBuffer.CollectionChanged +=
+                    (s, e) =>
+                    {
+                        this.logStringsDisplay.ScrollIntoView(this.logStringsDisplay.Items.GetItemAt(this.logStringsDisplay.Items.Count - 1));
+                    };
             }
+        }
+
+        public override void OnApplyTemplate()
+        {
+            base.OnApplyTemplate();
+
+            this.logStringsDisplay = this.Template.FindName("PART_LogStringsDisplay", this) as ListBox;
         }
         #endregion
     }
