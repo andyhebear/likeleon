f789dbe1025b9911e4c5af4d78a169b00c9758c0
likeleon <likeleon@a2a70008-d901-af86-2ea6-1ad5e87d510c>
2010-04-08 03:27:53 +0000

    Normal	completed	Settings 대화 상자 추가

---
+++ trunk/MogreTool/MogreEditor/Windows/SettingsDialog.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System.Windows;
+using Mogitor.Data;
+
+namespace Mogitor.Windows
+{
+    /// <summary>
+    /// Interaction logic for SettingsDialog.xaml
+    /// </summary>
+    public partial class SettingsDialog : Window
+    {
+        #region Properties
+        public ProjectOptions Options
+        {
+            get;
+            private set;
+        }
+        #endregion
+
+        public SettingsDialog(ProjectOptions options)
+        {
+            Options = options;
+
+            InitializeComponent();
+        }
+
+        private void btnOk_Click(object sender, RoutedEventArgs e)
+        {
+            this.DialogResult = true;
+        }
+
+        private void btnCancel_Click(object sender, RoutedEventArgs e)
+        {
+            this.DialogResult = false;
+        }
+    }
+}
