a5105a091646a8ba28582e9a01713efaace40c8a
likeleon <likeleon@a2a70008-d901-af86-2ea6-1ad5e87d510c>
2010-04-12 14:36:13 +0000

    Normal	incomplete	EditorObject처리해서, 프로젝트 import, export 동작 완료

--- MogreTool/MogreEditor/Data/MogitorsRoot.cs
+++ MogreTool/MogreEditor/Data/MogitorsRoot.cs
@@ -18,11 +18,6 @@ namespace Mogitor.Data
         /// </summary>
         private static MogitorsRoot instance;
         private readonly MogitorsSystem system;
-        private int objCounter = 0;
-        private readonly Hashtable nameList = new Hashtable();
-        private readonly List<Hashtable> namesByType = new List<Hashtable>((int)EditorType.LastEditor);
-        private readonly List<Hashtable> namesByTypeID = new List<Hashtable>(MogitorSettings.Instance.MaxObjectType);
-        private BaseEditor sceneManagerEditor = null;
         private Mogre.SceneManager sceneManager = null;
         #endregion
 
@@ -63,26 +58,6 @@ namespace Mogitor.Data
             get;
             private set;
         }
-
-        public BaseEditor SceneManagerEditor
-        {
-            get
-            {
-                if (this.sceneManagerEditor == null)
-                    this.sceneManagerEditor = FindObject(GetFirstSceneManager().Name, (int)EditorType.SceneManager);
-                return this.sceneManagerEditor;
-            }
-        }
-
-        public Mogre.SceneManager SceneManager
-        {
-            get
-            {
-                if (this.sceneManager == null)
-                    this.sceneManager = GetFirstSceneManager();
-                return this.sceneManager;
-            }
-        }
         #endregion
 
         #region Constructors
@@ -93,6 +68,11 @@ namespace Mogitor.Data
             RenderWindow = null;
             IsSceneLoaded = false;
             IsSceneModified = false;
+
+            RegisterAllEditorObjects();
+            BaseEditor nullParent = null;
+            this.rootEditor = BaseEditor.Factory.CreateObject(ref nullParent, null);
+
             ProjectOptions = new ProjectOptions();
         }
         #endregion
@@ -233,47 +213,6 @@ namespace Mogitor.Data
                 mngr.AddResourceLocation(resource, stype, group);
             }
         }
-
-        public string CreateUniqueID(string n1, string n2, int seed)
-        {
-            int temp = seed;
-
-            if (seed < 0)
-                seed = ++this.objCounter;
-
-            string tail = "#" + Mogre.StringConverter.ToString(seed);
-            if (n2 != "")
-            {
-                while (FindObject(n1 + tail, 0) != null || FindObject(n2 + tail, 0) != null)
-                {
-                    tail = "#" + Mogre.StringConverter.ToString(++seed);
-                }
-            }
-            else
-            {
-                while (FindObject(n1 + tail, 0) != null)
-                {
-                    tail = "#" + Mogre.StringConverter.ToString(++seed);
-                }
-            }
-
-            if (temp < 0)
-                this.objCounter = seed;
-
-            return tail;
-        }
-
-        public void UnRegisterObjectName(string name, BaseEditor obj)
-        {
-            if (this.nameList.ContainsKey(name))
-                this.nameList.Remove(name);
-
-            if (this.namesByType[(int)obj.EditorType].ContainsKey(name))
-                this.namesByType[(int)obj.EditorType].Remove(name);
-
-            if (this.namesByTypeID[(int)obj.ObjectTypeID].ContainsKey(name))
-                this.namesByTypeID[(int)obj.ObjectTypeID].Remove(name);
-        }
         #endregion
 
         #region Private Methods
@@ -284,30 +223,6 @@ namespace Mogitor.Data
             ProjectOptions.ProjectName = "";
             ProjectOptions.SceneManagerName = "";
             ProjectOptions.TerrainDirectory = "";
-        }
-
-        private BaseEditor FindObject(string name, uint type)
-        {
-            if (type == 0)
-            {
-                if (this.nameList.ContainsKey(name))
-                    return this.nameList[name] as BaseEditor;
-            }
-            else
-            {
-                if (this.namesByType[(int)type].ContainsKey(name))
-                    return namesByType[(int)type][name] as BaseEditor;
-            }
-            return null;
-        }
-
-        private Mogre.SceneManager GetFirstSceneManager()
-        {
-            IDictionaryEnumerator i = this.namesByType[(int)EditorType.SceneManager].GetEnumerator();
-            if (i != null)
-                return (i.Value as SceneManagerEditor).SceneManager;
-            else
-                return null;
         }
         #endregion
 
