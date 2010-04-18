c6ad57996b8e42be8352157d8b9c11b9ddb662c8
likeleon <likeleon@a2a70008-d901-af86-2ea6-1ad5e87d510c>
2010-04-18 02:52:58 +0000

    Normal	completed	Mogre.StringConverter.ParseVector2, Mogre.StringConverter.ParseVector4 구현

---
+++ trunk/MogreTool/MogreEditor/MogreX/StringConverter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Mogitor.MogreX
+{
+    class StringConverter
+    {
+        public static Mogre.Vector2 ParseVector2(string val)
+        {
+            string[] words = val.Split(delims);
+            if (words.Count() != 2)
+                return Mogre.Vector2.ZERO;
+            else
+                return new Mogre.Vector2(Mogre.StringConverter.ParseReal(words[0]),
+                                         Mogre.StringConverter.ParseReal(words[1]));
+        }
+
+        public static Mogre.Vector4 ParseVector4(string val)
+        {
+            string[] words = val.Split(delims);
+            if (words.Count() != 4)
+                return Mogre.Vector4.ZERO;
+            else
+                return new Mogre.Vector4(Mogre.StringConverter.ParseReal(words[0]),
+                                         Mogre.StringConverter.ParseReal(words[1]),
+                                         Mogre.StringConverter.ParseReal(words[2]),
+                                         Mogre.StringConverter.ParseReal(words[3]));
+        }
+
+        private static readonly char[] delims = new char[] {'\t', '\n', ' '};
+    }
+}
