58f4c872b9673404f0211a127f9bc5f1fc07d1a1
likeleon <likeleon@a2a70008-d901-af86-2ea6-1ad5e87d510c>
2010-05-06 12:38:09 +0000

    OBBoxRenderable 추가

--- MogreTool/Mogre/ogrenew/OgreMain/include/CLRConfig.h
+++ MogreTool/Mogre/ogrenew/OgreMain/include/CLRConfig.h
@@ -3,4 +3,4 @@
 //When compiling OgreMain for the first time LINK_TO_MOGRE should be 0 (since Mogre.dll doesn't
 //exist yet). After compiling OgreMain and Mogre, build OgreMain with LINK_TO_MOGRE = 1 so
 //that OgreMain can link to Mogre to instantiate Mogre's classes.
-#define LINK_TO_MOGRE 0
+#define LINK_TO_MOGRE 1
--- MogreTool/Mogre/ogrenew/OgreMain/src/OgreSimpleRenderable.cpp
+++ MogreTool/Mogre/ogrenew/OgreMain/src/OgreSimpleRenderable.cpp
@@ -33,6 +33,10 @@ Torus Knot Software Ltd.
 
 #include "OgreMaterialManager.h"
 
+#include "OgreHardwareVertexBuffer.h"
+#include "OgreHardwareBufferManager.h"
+#include "OgreTechnique.h"
+
 namespace Ogre {
 
     uint SimpleRenderable::ms_uGenNameCount = 0;
@@ -140,4 +144,278 @@ namespace Ogre {
         return queryLights();
     }
 
+	OBBoxRenderable::OBBoxRenderable(String matname, ColourValue colour)
+	{
+		mRenderOp.vertexData = new VertexData();
+		mRenderOp.indexData = 0;
+		mRenderOp.vertexData->vertexCount = 48;
+		mRenderOp.vertexData->vertexStart = 0;
+		mRenderOp.operationType = RenderOperation::OT_LINE_LIST;
+		mRenderOp.useIndexes = false;
+		VertexDeclaration* decl = mRenderOp.vertexData->vertexDeclaration;
+		VertexBufferBinding* bind = mRenderOp.vertexData->vertexBufferBinding;
+		decl->addElement(0, 0, VET_FLOAT3, VES_POSITION);
+		HardwareVertexBufferSharedPtr vbuf = HardwareBufferManager::getSingleton().createVertexBuffer(
+			decl->getVertexSize(0), mRenderOp.vertexData->vertexCount, HardwareBuffer::HBU_STATIC_WRITE_ONLY);
+		// Bind buffer
+		bind->setBinding(0, vbuf);
+		// setup material
+		ResourceManager::ResourceCreateOrRetrieveResult result = MaterialManager::getSingleton().createOrRetrieve(matname, "General");
+		if(result.second)
+		{
+			MaterialPtr matptrOBBoxManualMaterial = result.first;
+			matptrOBBoxManualMaterial->setReceiveShadows(false);
+			matptrOBBoxManualMaterial->getTechnique(0)->setLightingEnabled(true);
+			matptrOBBoxManualMaterial->getTechnique(0)->getPass(0)->setDiffuse(colour);
+			matptrOBBoxManualMaterial->getTechnique(0)->getPass(0)->setAmbient(colour);
+			matptrOBBoxManualMaterial->getTechnique(0)->getPass(0)->setSelfIllumination(colour);
+		}
+		this->setCastShadows(false);
+		this->setQueryFlags(0); // set a query flag to exlude from queries (if necessary).
+		this->setMaterial(matname);
+	}
+
+	OBBoxRenderable::~OBBoxRenderable()
+	{
+		delete mRenderOp.vertexData;
+	}
+
+	void OBBoxRenderable::setupVertices(const AxisAlignedBox& aab)
+	{
+		Vector3 vmax = aab.getMaximum();
+		Vector3 vmin = aab.getMinimum();
+		Real maxdistground = vmax.y;
+		Real mindistground = vmin.y;
+		Real maxx = vmax.x;
+		Real maxy = vmax.y;
+		Real maxz = vmax.z;
+		Real minx = vmin.x;
+		Real miny = vmin.y;
+		Real minz = vmin.z;
+
+		Real xdiff = (vmax.x - vmin.x) / 4.0f;
+		Real ydiff = (vmax.y - vmin.y) / 4.0f;
+		Real zdiff = (vmax.z - vmin.z) / 4.0f;
+
+
+		// fill in the Vertex buffer: 12 lines with 2 endpoints each make up a box
+		HardwareVertexBufferSharedPtr vbuf = mRenderOp.vertexData->vertexBufferBinding->getBuffer(0);
+		float* pPos = static_cast<float*>(vbuf->lock(HardwareBuffer::HBL_DISCARD));
+		// line 0
+		*pPos++ = minx;
+		*pPos++ = miny;
+		*pPos++ = minz;
+
+		*pPos++ = minx + xdiff;
+		*pPos++ = miny;
+		*pPos++ = minz;
+
+		*pPos++ = maxx - xdiff;
+		*pPos++ = miny;
+		*pPos++ = minz;
+
+		*pPos++ = maxx;
+		*pPos++ = miny;
+		*pPos++ = minz;
+		// line 1
+		*pPos++ = minx;
+		*pPos++ = miny;
+		*pPos++ = minz;
+
+		*pPos++ = minx;
+		*pPos++ = miny;
+		*pPos++ = minz + zdiff;
+
+		*pPos++ = minx;
+		*pPos++ = miny;
+		*pPos++ = maxz - zdiff;
+
+		*pPos++ = minx;
+		*pPos++ = miny;
+		*pPos++ = maxz;
+		// line 2
+		*pPos++ = minx;
+		*pPos++ = miny;
+		*pPos++ = minz;
+
+		*pPos++ = minx;
+		*pPos++ = miny + ydiff;
+		*pPos++ = minz;
+
+		*pPos++ = minx;
+		*pPos++ = maxy - ydiff;
+		*pPos++ = minz;
+
+		*pPos++ = minx;
+		*pPos++ = maxy;
+		*pPos++ = minz;
+		// line 3
+		*pPos++ = minx;
+		*pPos++ = maxy;
+		*pPos++ = minz;
+
+		*pPos++ = minx;
+		*pPos++ = maxy;
+		*pPos++ = minz + zdiff;
+
+		*pPos++ = minx;
+		*pPos++ = maxy;
+		*pPos++ = maxz - zdiff;
+
+		*pPos++ = minx;
+		*pPos++ = maxy;
+		*pPos++ = maxz;
+		// line 4
+		*pPos++ = minx;
+		*pPos++ = maxy;
+		*pPos++ = minz;
+
+		*pPos++ = minx + xdiff;
+		*pPos++ = maxy;
+		*pPos++ = minz;
+
+		*pPos++ = maxx - zdiff;
+		*pPos++ = maxy;
+		*pPos++ = minz;
+
+		*pPos++ = maxx;
+		*pPos++ = maxy;
+		*pPos++ = minz;
+		// line 5
+		*pPos++ = maxx;
+		*pPos++ = miny;
+		*pPos++ = minz;
+
+		*pPos++ = maxx;
+		*pPos++ = miny;
+		*pPos++ = minz + zdiff;
+
+		*pPos++ = maxx;
+		*pPos++ = miny;
+		*pPos++ = maxz - zdiff;
+
+		*pPos++ = maxx;
+		*pPos++ = miny;
+		*pPos++ = maxz;
+		// line 6
+		*pPos++ = maxx;
+		*pPos++ = miny;
+		*pPos++ = minz;
+
+		*pPos++ = maxx;
+		*pPos++ = miny + ydiff;
+		*pPos++ = minz;
+
+		*pPos++ = maxx;
+		*pPos++ = maxy - ydiff;
+		*pPos++ = minz;
+
+		*pPos++ = maxx;
+		*pPos++ = maxy;
+		*pPos++ = minz;
+		// line 7
+		*pPos++ = minx;
+		*pPos++ = maxy;
+		*pPos++ = maxz;
+
+		*pPos++ = minx + xdiff;
+		*pPos++ = maxy;
+		*pPos++ = maxz;
+
+		*pPos++ = maxx - xdiff;
+		*pPos++ = maxy;
+		*pPos++ = maxz;
+
+		*pPos++ = maxx;
+		*pPos++ = maxy;
+		*pPos++ = maxz;
+		// line 8
+		*pPos++ = minx;
+		*pPos++ = miny;
+		*pPos++ = maxz;
+
+		*pPos++ = minx;
+		*pPos++ = miny + ydiff;
+		*pPos++ = maxz;
+
+		*pPos++ = minx;
+		*pPos++ = maxy - ydiff;
+		*pPos++ = maxz;
+
+		*pPos++ = minx;
+		*pPos++ = maxy;
+		*pPos++ = maxz;
+
+		// line 9
+		*pPos++ = maxx;
+		*pPos++ = maxy;
+		*pPos++ = minz;
+
+		*pPos++ = maxx;
+		*pPos++ = maxy;
+		*pPos++ = minz + zdiff;
+
+		*pPos++ = maxx;
+		*pPos++ = maxy;
+		*pPos++ = maxz - zdiff;
+
+		*pPos++ = maxx;
+		*pPos++ = maxy;
+		*pPos++ = maxz;
+		// line 10
+		*pPos++ = maxx;
+		*pPos++ = miny;
+		*pPos++ = maxz;
+
+		*pPos++ = maxx;
+		*pPos++ = miny + ydiff;
+		*pPos++ = maxz;
+
+		*pPos++ = maxx;
+		*pPos++ = maxy - ydiff;
+		*pPos++ = maxz;
+
+		*pPos++ = maxx;
+		*pPos++ = maxy;
+		*pPos++ = maxz;
+		// line 11
+		*pPos++ = minx;
+		*pPos++ = miny;
+		*pPos++ = maxz;
+
+		*pPos++ = minx + xdiff;
+		*pPos++ = miny;
+		*pPos++ = maxz;
+
+		*pPos++ = maxx - xdiff;
+		*pPos++ = miny;
+		*pPos++ = maxz;
+
+		*pPos++ = maxx;
+		*pPos++ = miny;
+		*pPos++ = maxz;
+		vbuf->unlock();
+		// setup the bounding box of this SimpleRenderable
+		setBoundingBox(aab);
+	}
+
+	Real OBBoxRenderable::getSquaredViewDepth(const Camera* cam)const
+	{
+		Vector3 min, max, mid, dist;
+		min = mBox.getMinimum();
+		max = mBox.getMaximum();
+		mid = ((max - min) * 0.5) + min;
+		dist = cam->getDerivedPosition() - mid;
+		return dist.squaredLength();
+	}
+
+	Real OBBoxRenderable::getBoundingRadius()const
+	{
+		return 0;
+	}
+
+	void OBBoxRenderable::getWorldTransforms (Matrix4 *xform)const
+	{
+		SimpleRenderable::getWorldTransforms (xform);
+	}
 }
