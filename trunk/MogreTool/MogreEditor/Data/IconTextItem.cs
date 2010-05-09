94d6975e9b13fc57917cab47d670d7c1bbe392f8
likeleon <likeleon@a2a70008-d901-af86-2ea6-1ad5e87d510c>
2010-05-09 03:11:45 +0000

    Normal	completed	Common IconTextItem

---
+++ trunk/MogreTool/MogreEditor/Data/IconTextItem.cs
@@ -0,0 +1,28 @@
+﻿using System.Windows;
+
+namespace Mogitor.Data
+{
+    class IconTextItem : DependencyObject
+    {
+        public string Name
+        {
+            get { return (string)GetValue(NameProperty); }
+            set { SetValue(NameProperty, value); }
+        }
+
+        public string Icon
+        {
+            get;
+            private set;
+        }
+
+        public IconTextItem(string name, string icon)
+        {
+            Name = name;
+            Icon = icon;
+        }
+
+        public static readonly DependencyProperty NameProperty =
+            DependencyProperty.Register("Name", typeof(string), typeof(IconTextItem), new UIPropertyMetadata(null));
+    }
+}
