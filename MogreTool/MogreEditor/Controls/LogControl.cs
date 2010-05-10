cb98b1c4fa057c96b4933740351b11bfffaf84e5
likeleon <likeleon@a2a70008-d901-af86-2ea6-1ad5e87d510c>
2010-05-10 00:25:02 +0000

    Normal	completed	로그 컨트롤 무한대로 스크롤되는 문제 수정

--- MogreTool/MogreEditor/Controls/LogControl.cs
+++ MogreTool/MogreEditor/Controls/LogControl.cs
@@ -2,6 +2,7 @@
 using Mogitor.Data;
 using System.Windows.Controls;
 using System.ComponentModel;
+using System.Windows.Media;
 
 namespace Mogitor.Controls
 {
@@ -44,7 +45,12 @@ namespace Mogitor.Controls
             {
                 this.logStringsDisplay.Dispatcher.BeginInvoke((System.Action)(() =>
                     {
-                        this.logStringsDisplay.ScrollIntoView(e.NewItems[0]);
+                        // This code makes the listview scroll up-down and down-up in a infinite loop (in some systems?).
+                        //this.logStringsDisplay.ScrollIntoView(e.NewItems[0]);
+
+                        Decorator border = VisualTreeHelper.GetChild(this.logStringsDisplay, 0) as Decorator;
+                        ScrollViewer scroll = border.Child as ScrollViewer;
+                        scroll.ScrollToBottom();
                     }));
             }
         }
